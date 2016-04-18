5e8bba161649a2c3bb9bb0a9a52d2452cf41b72a
David Josefson <david.josefson@gmail.com>
2016-04-18 09:02:06 +0200

    Fixed path issue with node server

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/eats-client'));
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname));
 
 app.listen(4444, function() {
   console.log('Server for Angular client listening on port 4444!');
