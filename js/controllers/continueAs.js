34c309ba0901cbcc4cd00db575c559615cc9c8b0
Agetorp <elias.agetorp@gmail.com>
2016-04-25 16:34:24 +0200

    Continued working with continueAs

--- js/controllers/continueAs.js
+++ js/controllers/continueAs.js
@@ -1,12 +1,19 @@
 app.controller('continueAs', ['$scope', '$http', 'tokenService','$window', function($scope, $http, tokenService, $window) {
+  $scope.isLoggedInAsUser = tokenService.isUserWithValidToken();
+  $scope.isLoggedInAsAnonymous = tokenService.isAnonymousWithValidToken();
+  $scope.isUserWithInvalidToken = tokenService.isUserWithInvalidToken();
+  $scope.username = tokenService.getTokenUserName();
 
-  $scope.continueWithPresentTokenOrGetAnonymous = function() {
-    $http.defaults.headers.common['x-access-token'] = $window.localStorage['jwtToken']; //use the token
-    tokenService.testToken()  //Try to use the token to see if it's still valid
-    .then(function(response) {
-      console.log('You are now logged in as ' + $window.localStorage['userName']);
-    }).catch(function(response) {
-      tokenService.getAnonymousToken(); //The token was not valid, get a new one
-    });
+  $scope.continueAsAnonymous = function() {
+    if(!$scope.isLoggedInAsAnonymous){
+      tokenService.getAnonymousToken()
+      .then(function() {
+        $scope.hide();
+      });
+    } else {
+      $scope.hide();
+    }
   };
+
+  //continueAsUser()
 }]);
