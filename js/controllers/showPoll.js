62a5ec2fcb60c686b13c1353eca42238c10d9df2
Agetorp <elias.agetorp@gmail.com>
2016-04-25 16:33:51 +0200

    Improved showpoll-controller

--- js/controllers/showPoll.js
+++ js/controllers/showPoll.js
@@ -4,24 +4,26 @@
 app.controller('showPoll', ['$scope', '$http', 'pollService', function($scope, $http, pollService) {
   $scope.active = pollService.getActive();
 
-  $scope.getPollUsers = function() {
-    var completeUsers = [];
+  $scope.users = [];
+  $scope.restaurants = [];
+  $scope.votes = [];
+
+  $scope.extractUsersFromActivePoll = function() {
+    $scope.users = [];
     var users = $scope.active.data.relationships.users.data;
 
     for (var i = 0; i < users.length; i++) {
       for (var j = 0; j < $scope.active.included.length; j++) {
         if ($scope.active.included[j].id === users[i].id) {
-          completeUsers.push($scope.active.included[j]);
+          $scope.users.push($scope.active.included[j]);
         }
       }
     }
-    return completeUsers;
-
   };
 
-  $scope.getPollRestaurants = function() {
-    var completeRestaurants = [];
-    if(!$scope.active.data.relationships.restaurants){
+  $scope.extractRestaurantsFromActivePoll = function() {
+    $scope.restaurants = [];
+    if(!$scope.active.data.relationships.restaurants){ //Prevents error when poll is created without restaurants
       return [];
     }
     var restaurants = $scope.active.data.relationships.restaurants.data;
@@ -29,12 +31,44 @@ app.controller('showPoll', ['$scope', '$http', 'pollService', function($scope, $
     for (var i = 0; i < restaurants.length; i++) {
       for (var j = 0; j < $scope.active.included.length; j++) {
         if ($scope.active.included[j].id === restaurants[i].id) {
-          completeRestaurants.push($scope.active.included[j]);
+          $scope.restaurants.push($scope.active.included[j]);
         }
       }
     }
-    return completeRestaurants;
-
   };
+
+  $scope.extractVotesFromActivePoll = function() {
+    $scope.votes = [];
+    for (let i = 0; i < $scope.active.included.length; i++) {
+
+      if ($scope.active.included[i].type === 'vote') {
+        $scope.votes.push({
+          user: $scope.active.included[i].relationships.user.data.id,
+          restaurant: $scope.active.included[i].relationships.restaurant.data.id
+        });
+      }
+    }
+  };
+
+  $scope.vote = function(restaurant) {
+    $http({
+      method: 'POST',
+      url: 'http://128.199.48.244:7000/polls/' + $scope.active.data.id + '/votes',
+      headers: {'Content-Type': 'application/json'},
+      data: {
+        restaurantId : restaurant
+      }
+    }).then(function (response) {
+      alert('Bravo, du röstade! När detta händer ska realtidsuppdateringar visa din röst. Tills dess; uppdatera sidan!');
+      $scope.extractVotesFromActivePoll();
+    }).catch(function (error) {
+      console.log('error');
+      console.log(error);
+    })
+  };
+
+  $scope.extractRestaurantsFromActivePoll();
+  $scope.extractUsersFromActivePoll();
+  $scope.extractVotesFromActivePoll();
 
 }]);
