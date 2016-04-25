477efaedd25d49fc19c96d21d34ed58452d21d8f
Agetorp <elias.agetorp@gmail.com>
2016-04-25 11:51:16 +0200

    Changed so that loginUser-page asks user if he/she wants to continue as anon or login

--- js/api/loginUser.js
+++ js/api/loginUser.js
@@ -4,35 +4,32 @@ app.controller('loginUser', function($scope, $http, $window) {
             'email' : $scope.email,
             'password' : $scope.password
             };
-            
+
             $http({
                 method: 'POST',
                 url: 'http://128.199.48.244:7000/auth',
                 headers: {'Content-Type': 'application/json'},
                 data: user
             }).then(function successCallback(response){
-                
+
                 var userData = response.data;
-             
+
                 var message = response.data.message;
                 $window.localStorage['userAnon'] = userData.anon;
                 $window.localStorage['userName'] = userData.name;
                 $window.localStorage['jwtToken'] = userData.token;
-                
+
                 $window.location.reload();
             }, function errorCallback(){
                 console.log('error');
-            });            
+            });
         };
-    
+
         $scope.logoutUser = function(){
             $window.localStorage.removeItem('jwtToken');
             $window.localStorage.removeItem('userAnon');
             $window.localStorage.removeItem('userName');
-            $window.location.reload();
+            $scope.dialogs.showAdvanced(null, 'continueAs');
         }
-        
+
 });
-
-
-
