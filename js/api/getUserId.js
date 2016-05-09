ea781dce0812f061a34ef32ca14a33a5b61a733a
Marcus Fröling <marcus.froling@gmail.com>
2016-05-09 13:07:55 +0200

    js files changed, data is working

--- js/api/getUserId.js
+++ js/api/getUserId.js
@@ -1,25 +1,18 @@
 //TODO: TEST THIS API FUNCTION, MAYBE RESTRUCTURE RESULT DATA FOR APP.USAGE
 // Not sure if this needs to be in a angular controller, if only called by other js-files
 app.controller('getUserId', ['$scope', '$http', '__env', function($scope, $http, __env) {
-  var userIdResult = {};
-
-  $scope.getUserId = function(ID) {
+  var userIdResult = getUserId(localStorage.getItem("userId"));
+  $scope.userIdData = userIdResult;
+  function getUserId(ID) {
     var id = ID;
     var link = __env.API_URL + '/users/' + new String(id);
-
     $http({
       method: 'GET',
       url: link
     }).then(function successCallback(response) {
-      resultUser(response.data);
+      $scope.data = response;
+      console.log(response.data);
     }, function errorCallback(response) {
       console.log("error");
     });
-
-    function resultUser(response) {
-      userIdResult = response;
-      console.log(userIdResult);
-      return userIdResult;
-    }
-  }
-}]);
+}}]);
