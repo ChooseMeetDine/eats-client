b73856b551cfc905980f4e4f857fbfd1b730260b
Agetorp <elias.agetorp@gmail.com>
2016-05-10 09:19:13 +0200

    Started implementing displaying of popup when polls expire

--- js/controllers/activePolls.js
+++ js/controllers/activePolls.js
@@ -2,12 +2,26 @@ app.controller('activePolls', ['$scope', 'pollService', '$http', '$q', '$window'
   $scope.polls = pollService.getAll();
   parameterPollId = $location.search().poll; // poll ID from URL
 
-  $scope.now = new Date();
-
   // Updates the date every second to be able to compare to the expiration date of the poll
   $interval(function() {
     $scope.now = new Date();
   }, 1000);
+
+  //For each poll that has not exipred when page is loaded, set a timeout that shows a popup when poll has exipred
+  var showPopupWhenPollExipres = function(poll){
+      if($scope.now < poll.data.expiresAsDateObj) {
+
+        setTimeout(function(){
+          var active = pollService.getActive();
+          if(!active.raw.data || active.raw.data.id !== poll.data.id) {
+            pollService.setActiveId(poll.data.id);
+            console.log(poll.data.id)
+            $scope.dialogs.showPopup(null, 'showActivePoll', true, true);
+          }
+        }, 10);
+        // }, poll.data.expiresAsDateObj - new Date());
+      }
+  };
 
   $scope.switchActivePoll = function(poll) {
     pollService.setActiveId(poll.data.id);
@@ -46,6 +60,8 @@ app.controller('activePolls', ['$scope', 'pollService', '$http', '$q', '$window'
       url: __env.API_URL + '/polls/' + pollId
     }).then(function(response) {
       pollService.add(response.data);
+      showPopupWhenPollExipres(response.data);
+
     });
   };
 
