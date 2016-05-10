592408c9906d69b2451c922a1878b9bfe6cff4f0
John George <bravelygeorge@gmail.com>
2016-05-10 11:03:48 +0200

    filter function now works with updated feature

--- js/controllers/filterController.js
+++ js/controllers/filterController.js
@@ -1,49 +1,31 @@
 app.controller('filterController', ['$scope', 'filterService', function($scope, filterService){
     //Sets all checbox to true separately
     $scope.truthy = [true, true, true, true, true, true, true, true, true, true];
-    //console.log($scope.truthy);
-    $scope.toggle = function(categoryId){
-        //console.log(categoryId)
-        //console.log($scope.truthy);
+    //Category array with te purpose of determinating if category has been clicked.
+    categoryClicked = [];
+    
+    $scope.toggle = function(categoryId){   
         
+        //Toggle filter once
         filterService.toggleFilter(categoryId);
-        
-        Array.prototype.allValuesSame = function(){
-            for(var i = 1; i < this.length; i++){
-                if(this[i] !== this[0]){
-                    return false;
-                }
-            }
-            return true;
-        }
-        
+         
+        //function set to array object that sets all positions of array to same value.
         Array.prototype.setAll = function(value){
-            //var i, n = this.length;
             for(var i = 0; i < this.length; ++i){
                 this[i] = value;
             }
         }
         
-        
-        if($scope.truthy[categoryId-1] == false){
-            console.log('bajs');
+        //Condition that removes all filters first time user clicks on category
+        if($scope.truthy[categoryId-1] == false && categoryClicked.indexOf(categoryId) == -1 && categoryClicked.length < 1){
             $scope.truthy.setAll(false);
             $scope.truthy[categoryId-1] = true;
-            for(var i = 0; i < $scope.truthy.length; i++){
-                 filterService.toggleFilter(i+1);
-            }
             
-            //console.log($scope.truthy);
+            categoryClicked.push(categoryId);//push category
+            
+            for(var i = 0; i < $scope.truthy.length; i++){
+                    filterService.toggleFilter(i+1);
+            }
         }
-        /*
-        var copyTruthy = $scope.truthy;
-        copyTruthy.slice(categoryId-1,  )*/
-        var list = [false,false,true,false,false];
-        Array.prototype.except = function(val){
-            return this.filter(function(x) { return x !== val; }); 
-        }
-        console.log(list.except(true));
     }
-   
-   
 }]);
