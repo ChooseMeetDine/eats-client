fc50a25e574a5a1ecf00db48c525a93cb0690c42
John George <bravelygeorge@gmail.com>
2016-05-19 14:06:28 +0200

    added easter-egg to console

---
+++ js/easter-egg.js
@@ -0,0 +1,11 @@
+//console.log($scope);
+//var consoleCss = 'color:#00B8D4; background:#424242; font-family:courier; font-size:25px; text-align:center';
+var consoleCss = 'color:#00B8D4; font-family:courier; font-size:25px; text-align:center';
+//console.log('%c > Eats är en betatjänst skapad av: \n > Amanda, Andreas, David, Elias, Gustav,\n > Jenny, Marcus, Musse, Natalia och Patrik', consoleCss);
+//console.log('%c > cd eats\n > ls \n > Amanda\n   Andreas\n   David\n   Elias\n   Gustav\n   Jenny\n   Marcus\n   Musse\n   Natalia\n   Patrik\n > npm start\n > ... http://localhost:4444', consoleCss);
+console.log('%c   _/_/_/_/    _/_/    _/_/_/_/_/    _/_/_/\n'+
+            '   _/        _/    _/      _/      _/\n'+          
+           '  _/_/_/    _/_/_/_/      _/        _/_/\n'+       
+          ' _/        _/    _/      _/            _/\n'+   
+        '_/_/_/_/  _/    _/      _/      _/_/_/\n  ', consoleCss);
+console.log('%c > Eats är en betatjänst skapad av: \n > Amanda, Andreas, David, Elias, Gustav,\n > Jenny, Marcus, Musse, Natalia och Patrik', consoleCss);
